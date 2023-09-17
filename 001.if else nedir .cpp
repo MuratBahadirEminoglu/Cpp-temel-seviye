281370af8cc6ad405e03083dd8424f96612fe71b
MuratBahadirEminoglu <141485967+MuratBahadirEminoglu@users.noreply.github.com>
2023-09-17 13:43:57 +0300

    Update 001.if else nedir .cpp

--- 001.if else nedir .cpp
+++ 001.if else nedir .cpp
@@ -1,1 +1,14 @@
+/* if else nedir nasıl çalışır 
+Koşula bağlı olarak tek bir işlemi yerine getiren , yani şartlı dallanma işlemşnş gerçekleştiren yapıdır. Hemen hemen tüm programlama dillerinde bu işlem 
+için if deyimi kullanılır. tek seçimli yapılara örnek verirse;
+1- eger yemek pişti ise ocağın altını kapat 
+2- eger sıcaklık 0 derecenin altında ise donma olabilir
 
+tek seçimli yapılarda if deyiminin kullanım şekli ve çalışma mantığı;
+if( koşul)                if(koşul)
+{                           işlem;  
+işlem;
+}
+
+kullanıl şeklinden de görüleceği üzere if yapısında yapılacak işlemler istenirse {} süslü parantez blokları arasına alınır. eğer if yada else'den sonra sadece tek bir komut satırı
+  yazlıacaksa aç-kapa{...} süslü parantezleri kullanmayabilir. eğer koşul kısmında birden fazla değerin karşılaştırması yapılacaksa arada VE(&&) VEYA(||) operatörleri kullanılabilir.
