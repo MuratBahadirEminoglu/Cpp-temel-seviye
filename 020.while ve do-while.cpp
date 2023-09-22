fc6c0fb14f901ea69cf9b96ea218a02ae3670442
MuratBahadirEminoglu <141485967+MuratBahadirEminoglu@users.noreply.github.com>
2023-09-22 22:20:51 +0300

    020.while ve do-while.cpp

---
+++ 020.while ve do-while.cpp
@@ -0,0 +1,16 @@
+/* işlemlerin ne kadar tekrarlanacağının baştan belli olmadığı ancak bir koşulun doğru ya da yanlış
+olmasına bağlı olarak tekrar sayısının belirlendiği bu döngüleri, koşul kontrolünün yapıldığı yere göre
+while ve do while olmak üzere ikiye ayrılır. 
+
+1- while dögüleri , koşul kontrolünün döngünün başında yapıldığı yapılardır.
+2- do while döngüleri , koşul kontrolünün döngünün sonunda yapıldığı yapılardır.
+  Döngü içerisinde bir defa girildikten sonra koşul kontrolü yapılı. ,koşulun 
+doğru yada yanlış olmasına göre de işlem tekrarı yapılır */
+/*
+  while yapisi                   do-while yapisi
+while(koşul){                    do{
+işlem1;                             işlem1;
+işlem2;                             işlem2;
+.......                             .........
+  }                                }while(koşul);
+*/
