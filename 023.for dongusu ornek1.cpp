e91b77cb4a02f2afd8b8a3f2943fdab96d2893b5
MuratBahadirEminoglu <141485967+MuratBahadirEminoglu@users.noreply.github.com>
2023-10-08 14:01:32 +0300

    023.for dongusu 0 dan 15 e kadar olan tek sayilari yazdirma 1.cpp

---
+++ 023.for dongusu ornek1.cpp
@@ -0,0 +1,10 @@
+/ * 3 den 15 e kadar tek sayilari ekranda gösteren program program */
+  for(int = ; i <= 15 ; i++){
+  cout << i << "\t";
+  }
+//veya 
+  for( int i= 3; i <= 15; i++){
+  if(i %3 == 0){
+    cout << i << "\t";
+  }
+  }
